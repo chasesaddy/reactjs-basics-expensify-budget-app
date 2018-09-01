5325bebf5495c099b3bc2b5f70be0ed07107966b
Saadh <atextbooksituation@gmail.com>
2018-09-01 12:04:24 -0400

    Up to — Section 9, Lecture 77: "React-Router 101"

--- src/app.js
+++ src/app.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import 'normalize.css/normalize.css';
 
 import './styles/styles.scss';
 
-ReactDOM.render( <p>Boilerplate</p>, document.getElementById( 'app' ) );
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from my dashboard component
+    
+    <ul>
+      <li>
+        <a href="/create">Create</a>
+      </li>
+      <li>
+        <a href="/edit">Edit</a>
+      </li>
+      <li>
+        <a href="/help">Help</a>
+      </li>
+    </ul>
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from my add expense component
+  </div>
+);
+
+const EditExpensePage = () => (
+  <div>
+    This is the edit expense component
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    The help page.
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>  
+    <div>
+      <Route path="/" component={ ExpenseDashboardPage } exact={ true } />
+      <Route path="/create" component={ AddExpensePage } exact={ true } />
+      <Route path="/edit" component={ EditExpensePage } exact={ true } />
+      <Route path="/help" component={ HelpPage } exact={ true } />
+    </div>
+  </BrowserRouter>
+);
+
+ReactDOM.render( routes, document.getElementById( 'app' ) );
