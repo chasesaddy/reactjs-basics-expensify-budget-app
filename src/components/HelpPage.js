bf4a3797366680b6cf4b8ac5797a78c36d523bfa
Saadh <atextbooksituation@gmail.com>
2018-09-02 19:14:05 -0400

    Up to — Section 9, Lecture 81: "Query Strings and URL Parameters"

---
+++ src/components/HelpPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HelpPage = () => (
+  <div>
+    The help page.
+  </div>
+);
+
+export default HelpPage;
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+import Dashboard from '../components/Dashboard';
+import Add from '../components/Add';
+import Edit from '../components/Edit';
+import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
+
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header />
+      <Switch>
+        <Route path="/" component={ Dashboard } exact={true} />
+        <Route path="/create" component={ Add } exact={true} />
+        <Route path="/edit" component={ Edit } exact={true} />
+        <Route path="/help" component={ HelpPage } exact={true} />
+        <Route component={ NotFoundPage } />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+export default AppRouter;
