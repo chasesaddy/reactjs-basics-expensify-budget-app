27ebcc45c9006ed0e11a2190935692d07829dee0
Saadh <atextbooksituation@gmail.com>
2018-09-10 07:24:50 -0400

    Up to — Section 11, Lecture 108: "Wiring up Edit Expense"

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import ExpenseForm from './ExpenseForm';
+import { editExpense, removeExpense } from '../actions/expenses';
 
 const EditExpensePage = ( props ) => (
   <div>
-    Editing expense with id of { props.match.params.id }
+    <ExpenseForm 
+      expense={ props.expense }
+      onSubmit={ ( oneExpense ) => {
+        props.dispatch( editExpense( props.expense.id, oneExpense ) );
+        props.history.push( '/' );
+      }}
+    />
+    <button onClick={ () => {      
+      const theId = props.expense.id;
+      console.log( 'props expense', { id: theId } );
+      props.dispatch( removeExpense( { id: theId } ) );
+      props.history.push( '/' );
+    }}>
+      Remove
+    </button>
   </div>
 );
 
-export default EditExpensePage;
+const mapStateToProps = ( state, props ) => {
+  return {
+    expense: state.expenses.find( ( oneExpense ) => oneExpense.id === props.match.params.id )
+  };
+};
+
+export default connect( mapStateToProps )( EditExpensePage );
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-import { removeExpense } from '../actions/expenses';
-
-const ExpenseListItem = ( { dispatch, id, description, amount, createdAt } ) => (
+const ExpenseListItem = ( { id, description, amount, createdAt } ) => (
   <div>
-    <h3>{ description }</h3>
+    <h3>
+      <Link to={ `${ id }/edit` }>{ description }</Link>
+    </h3>
     <p>{ amount } - { createdAt }</p>    
-    <button onClick={ () => {
-      dispatch( removeExpense( { id } ) );
-    }}>
-      Remove
-    </button>
   </div>
 );
 
-const mapStateToProps = ( state ) => {
-  return {
-    expenses: state.expenses
-  };
-};
-
-export default connect( mapStateToProps )( ExpenseListItem );
+export default ExpenseListItem;
