a06b184cef0d1d4976c170f126655be1d29d6ab5
Saadh <atextbooksituation@gmail.com>
2018-09-12 12:37:14 -0400

    Up to — Section 14, Lecture 150: "Array Data in Firebase: Part II" <14:56> [00:45]

--- src/playground/firebase/firebase-arrays.js
+++ src/playground/firebase/firebase-arrays.js
@@ -6,3 +6,38 @@ db.ref( 'expenses' ).push( {
   description: 'lol',
   amount: 5000
 } );
+
+// To get array back, convert firebase content into an array
+db.ref( 'expenses' ).on( 'value', ( snapshot ) => { 
+  const expenses = [];
+
+  snapshot.forEach( ( solo ) => { 
+    expenses.push( { 
+      id: solo.key,
+      ...solo.val()
+    } );
+  } );
+
+  console.log( expenses );
+} );
+
+
+//
+// Other examples of methods to see changes
+
+// child_removed lets you know when a child from the root of the ref() gets removed
+db.ref( 'expenses' ).on( 'child_removed', ( snapshot ) => { 
+  console.log( snapshot.key, snapshot.val() );
+} );
+
+// child_changed as it sounds like, lets you know a child from the root of the ref() gets changed
+db.ref( 'expenses' ).on( 'child_changed', ( snapshot ) => { 
+  console.log( snapshot.key, snapshot.val() );
+} );
+
+// child_added - again as it sounds. Fires when child from the root of the ref() gets added
+// child_addded doesn't only get called for when a child is added. It does do that.
+// It also fires for all initial children at the ref() location
+db.ref( 'expenses' ).on( 'child_added', ( snapshot ) => { 
+  console.log( snapshot.key, snapshot.val() );
+} );
