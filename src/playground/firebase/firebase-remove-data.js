696ca4dbaf16f5a7e2c4f5e39ccf6d2a60207cd0
Saadh <atextbooksituation@gmail.com>
2018-09-12 10:05:35 -0400

    Up to — Section 14, Lecture 146: "Removing Data from Firebase" <5:42> [00:17]

--- src/playground/firebase/firebase-remove-data.js
+++ src/playground/firebase/firebase-remove-data.js
@@ -1,2 +1,15 @@
 import db from './firebase-config';
 
+// main way to remove
+db.ref( 'isSingle' ).remove().then( () => { 
+  console.log( 'Success. Removed.' );
+} ).catch( ( e ) => { 
+  console.log( 'Remove failed: ', e );
+} );
+
+// other way
+db.ref( 'isSingle' ).set( null ).then( () => { 
+  console.log( 'Success. Removed.' );
+} ).catch( ( e ) => { 
+  console.log( 'Remove failed: ', e );
+} );
