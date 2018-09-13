fa32291a78f46a5a6ed3dd9885a1a1372f137aba
Saadh <atextbooksituation@gmail.com>
2018-09-13 13:41:15 -0400

    Up to — Section 12, Lecture 117: "Testing Expenses Reducer" <15:10> [00:45]

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,89 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses'
+
+test( 'should set default value', () => { 
+  const state = expensesReducer( undefined, { type: '@@INT' } );
+
+  expect( state ).toEqual( [] );
+} );
+
+test( 'should remove expense by id', () => { 
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: expenses[ 1 ].id
+  };
+
+  const state = expensesReducer( expenses, action );
+  expect( state ).toEqual( [ expenses[ 0 ], expenses[ 2 ] ] );
+} );
+
+test( 'should not remove expense if id is not found', () => { 
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '987987'
+  };
+
+  const state = expensesReducer( expenses, action );
+  expect( state ).toEqual( expenses );
+} );
+
+
+// add expense
+test( 'should add expense', () => { 
+  const expense = {
+    description: 'another one',
+    note: 'here we go',
+    amount: 10,
+    createdAt: 50
+  }
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense
+  };
+
+  const state = expensesReducer( expenses, action );
+
+  // Proper and best way. Didn't think of it!
+  expect( state ).toEqual( [ ...expenses, expense ] );
+  // My ways i thought of
+  expect( state ).toHaveLength( 4 );
+  expect( state[ state.length - 1 ].description ).toEqual( 'another one' );
+} )
+
+
+// edit expense
+// don't edit if expense not found (id)
+test( 'should edit expense', () => { 
+  const updates = {
+    description: 'and another one',
+    note: 'and here we go',
+    amount: 110,
+    createdAt: 150
+  }
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[ 1 ].id,
+    updates
+  };
+
+  const state = expensesReducer( expenses, action );
+  expect( state[ 1 ] ).not.toEqual( expenses[ 1 ] );
+  expect( state[ 1 ].description ).toBe( 'and another one' );
+} );
+
+test( 'should not edit expense if [id] not found', () => { 
+  const updates = {
+    description: 'yo and another one',
+    note: 'yo and here we go',
+    amount: 9110,
+    createdAt: 9150
+  };
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '789789',
+    updates
+  };
+
+  const state = expensesReducer( expenses, action );
+  expect( state ).toEqual( expenses );
+} );
--- src/tests/selectors/expenses.test.js
+++ src/tests/selectors/expenses.test.js
@@ -1,29 +1,7 @@
-import selectExpenses from '../../selectors/expenses';
-
 import moment from 'moment';
 
-const expenses = [
-  {
-    id: '1',
-    description: 'Gumming',
-    note: 'Shiz just keeps on...',
-    amount: 350,
-    createdAt: moment( 0 ).subtract( 4, 'days' ).valueOf()
-  }, {
-    id: '2',
-    description: 'Yummy',
-    note: 'Deluxe package espana',
-    amount: 1790,
-    createdAt: moment( 0 ).add( 4, 'days' ).valueOf()
-  },
-  {
-    id: '3',
-    description: 'So weak',
-    note: 'meh',
-    amount: 6000,
-    createdAt: 10000
-  }
-]
+import selectExpenses from '../../selectors/expenses';
+import expenses from '../fixtures/expenses'
 
 const def = {
   text: '',
