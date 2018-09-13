2e0652656f7211e44d9da22df9faa70e564b1fc4
Saadh <atextbooksituation@gmail.com>
2018-09-13 07:48:22 -0400

    Up to — Section 12, Lecture 112: "Setting up Jest" <19:34> [00:59] — Had to do a bit of additional setup installing on top of course

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
     },
     "extends": [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
+        "plugin:jest/recommended"
     ],
     "parserOptions": {
         "ecmaFeatures": {
@@ -17,7 +18,8 @@ module.exports = {
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "jest"
     ],
     "rules": {
         "indent": [
