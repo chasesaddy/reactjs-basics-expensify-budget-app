4f368d5ba40f690dfa2170fe6a0487ef19038be6
Saadh <atextbooksituation@gmail.com>
2018-09-14 11:48:42 -0400

    Up to — Section 12, Lecture 122: "Testing User Interaction" <16:54> [00:51]
    In general, don't like these tests much. They are checking for things that might work in tests, but not for the user. User using browser doesn't care if tests work, but doesn't work for them in browser. The last invalid amount is a good example of this being silly for end user as it's checking something that's not even possible for the user to do. It's just testing internal functions. Which is fine, just not as the only test. There should be a user behavior test using browser to be sure entering invalid amount isn't even possible as that is the case. So '12.122' isn't even possible to enter. It would end up just being '12.22'.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -20,6 +20,55 @@ test( 'should render ExpenseForm correctly with data', () => {
 test( 'should render error for invalid form submission', () => { 
   const wrapper = shallow( <ExpenseForm /> );
 
+  expect( wrapper ).toMatchSnapshot();
+
   wrapper.find( 'form' ).simulate( 'submit', { preventDefault: () => {} } );
+
+  expect( wrapper.state( 'error' ).length ).toBeGreaterThan( 0 );
+  expect( wrapper ).toMatchSnapshot();
 } );
 
+test( 'should set description on input change', () => { 
+  const value = 'New description';
+  const wrapper = shallow( <ExpenseForm /> );
+
+  // I don't like using at. Would much prefer better specific thing like ID. Or class if it'll be unique. Like scraping.
+  wrapper.find( 'input' ).at( 0 ).simulate( 'change', { 
+    target: { value } 
+  } );
+  
+  expect( wrapper.state( 'description' ) ).toBe( value ); 
+} );
+
+test( 'should set note on input change', () => {
+  const value = 'New note';
+  const wrapper = shallow( <ExpenseForm /> );
+
+  wrapper.find( 'textarea' ).simulate( 'change', {
+    target: { value }
+  });
+
+  expect( wrapper.state( 'note' ) ).toBe( value );
+} );
+
+test( 'should set valid amount on input change', () => {
+  const value = '23.50';
+  const wrapper = shallow( <ExpenseForm /> );
+
+  wrapper.find( 'input' ).at( 1 ).simulate( 'change', {
+    target: { value }
+  });
+
+  expect( wrapper.state( 'amount' ) ).toBe( value );
+} );
+
+test( 'should try to set invalid amount on input change', () => {
+  const value = '12.222';
+  const wrapper = shallow( <ExpenseForm /> );
+
+  wrapper.find( 'input' ).at( 1 ).simulate( 'change', {
+    target: { value }
+  });
+
+  expect( wrapper.state( 'amount' ) ).toBe( '' );
+} );
