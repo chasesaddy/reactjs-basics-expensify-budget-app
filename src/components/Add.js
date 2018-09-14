0d25bae02cb250288f2ded3afdebb7843eca48d3
Saadh <atextbooksituation@gmail.com>
2018-09-14 19:50:30 -0400

    Moving content around to make it able to be tested. onSubmit arrow function goes into onSubmit method. AddExpensePage becomes a React.Component class. Rendering mvoed to render(). And most of all, mapsDispatchToProps is added.

--- src/components/Add.js
+++ src/components/Add.js
@@ -4,19 +4,29 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { addExpense } from '../actions/expenses';
 
-const AddExpensePage = ( props ) => (
-  <div>
-    <h1>
-      Add Expense
-    </h1>
+export class AddExpensePage extends React.Component {
+  onSubmit = ( oneExpense ) => {    
+    this.props.onSubmit( oneExpense );
+    this.props.history.push( '/' );
+  };
 
-    <ExpenseForm 
-      onSubmit={ ( oneExpense ) => {
-        props.dispatch( addExpense( oneExpense ) );
-        props.history.push( '/' );
-      }}
-    />
-  </div>
-);
+  render() {
+    return (
+      <div>
+        <h1>
+          Add Expense
+        </h1>
 
-export default connect()( AddExpensePage );
+        <ExpenseForm 
+          onSubmit={ this.onSubmit }
+        />
+      </div>
+    );
+  };
+}
+
+const mapsDispatchToProps = ( dispatch ) => ( {
+  onSubmit: ( expense ) => dispatch( addExpense ( expense ) )
+} );
+
+export default connect( undefined, mapsDispatchToProps )( AddExpensePage );
