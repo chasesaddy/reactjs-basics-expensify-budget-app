2394528a683e5537fa8521588f4e7522e5036e30
Saadh <atextbooksituation@gmail.com>
2018-09-14 10:27:40 -0400

    Minor edits

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 
 import Header from '../../components/Header';
 
-test( 'should render Header correctly', () => { 
+test( 'should render [simple] Header correctly', () => { 
   const wrapper = shallow( <Header /> );
   
   expect( wrapper ).toMatchSnapshot();
