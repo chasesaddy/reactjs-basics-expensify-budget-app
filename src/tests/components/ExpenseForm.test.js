c4cdfbf62dbb83e9b886d43039f4045fc3aa7d8f
Saadh <atextbooksituation@gmail.com>
2018-09-14 12:24:50 -0400

    Up to — Section 12, Lecture 123: "Test Spies" <17:39> [00:53]
    Okay I get why these test are being done now. They make sense to have them sometimes. Depends on what is being done in code vs what user is doing.

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -4,6 +4,8 @@ import { shallow } from 'enzyme';
 import ExpenseForm from '../../components/ExpenseForm';
 import expenses from '../fixtures/expenses';
 
+import moment from 'moment';
+
 test( 'should render ExpenseForm correctly', () => { 
   const wrapper = shallow( <ExpenseForm /> );
 
@@ -22,7 +24,9 @@ test( 'should render error for invalid form submission', () => {
 
   expect( wrapper ).toMatchSnapshot();
 
-  wrapper.find( 'form' ).simulate( 'submit', { preventDefault: () => {} } );
+  wrapper.find( 'form' ).simulate( 'submit', { 
+    preventDefault: () => { } 
+  } );
 
   expect( wrapper.state( 'error' ).length ).toBeGreaterThan( 0 );
   expect( wrapper ).toMatchSnapshot();
@@ -71,4 +75,53 @@ test( 'should try to set invalid amount on input change', () => {
   });
 
   expect( wrapper.state( 'amount' ) ).toBe( '' );
+} );
+
+test( 'should call onSubmit prop for dummy form submission', () => { 
+  const onSubmitSpy = jest.fn();
+  onSubmitSpy( 'Moi', 'Phillah' );
+  expect( onSubmitSpy ).toHaveBeenCalledWith( 'Moi', 'Phillah' );
+} );
+
+test( 'should call onSubmit prop for valid form submission', () => { 
+  const example = expenses[ 0 ];
+  const onSubmitSpy = jest.fn();
+  const wrapper = shallow( <ExpenseForm expense={ example } onSubmit={ onSubmitSpy } /> );
+  wrapper.find( 'form' ).simulate( 'submit', { 
+    preventDefault: () => { } 
+  } );
+
+  expect( wrapper.state( 'error' ) ).toBe( '' );
+  expect( onSubmitSpy ).toHaveBeenLastCalledWith( { 
+    description: example.description,
+    note: example.note,
+    amount: example.amount,
+    createdAt: example.createdAt
+  } );
+} );
+
+test( 'should set new data on date change', () => { 
+  const now = moment();
+  const wrapper = shallow( <ExpenseForm /> );
+
+  console.log( wrapper.debug() );
+
+  // first way to make SingleDatePicker still not work as opposed to course not having an issue. This was from enzyme issue 184 comments.
+  // wrapper.find( SingleDatePicker ).prop( 'onDateChange' )( now );
+  // other way to make it work as opposed to course
+  wrapper.find( 'withStyles(SingleDatePicker)' ).prop( 'onDateChange' )( now );
+
+  expect( wrapper.state( 'createdAt' ) ).toEqual( now );
+} );
+
+test( 'should set proper data on focus change', () => { 
+  const focused = true;
+  const wrapper = shallow( <ExpenseForm /> );
+
+  // first way to make SingleDatePicker still not work as opposed to course not having an issue. This was from enzyme issue 184 comments.
+  // wrapper.find( SingleDatePicker ).prop( 'onFocusChange' )( theBool );
+  // other way to make it work as opposed to course
+  wrapper.find( 'withStyles(SingleDatePicker)' ).prop( 'onFocusChange' )( { focused } );
+
+  expect( wrapper.state( 'calFocused' ) ).toEqual( focused );
 } );
