4939bb30d7ab6f6c6637c8aebab68349eff5932e
Saadh <atextbooksituation@gmail.com>
2018-09-14 20:55:08 -0400

    Part of — Section 12, Lecture 125: "Testing EditExpensePage" <19:00> [00:57] — Refactoring the Edit Expense Page like Add. Getting the hang of it now. But still needed to look back a bit

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,30 +4,45 @@ import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm';
 import { editExpense, removeExpense } from '../actions/expenses';
 
-const EditExpensePage = ( props ) => (
-  <div>
-    <ExpenseForm 
-      expense={ props.expense }
-      onSubmit={ ( oneExpense ) => {
-        props.dispatch( editExpense( props.expense.id, oneExpense ) );
-        props.history.push( '/' );
-      }}
-    />
-    <button onClick={ () => {      
-      const theId = props.expense.id;
-      console.log( 'props expense', { id: theId } );
-      props.dispatch( removeExpense( { id: theId } ) );
-      props.history.push( '/' );
-    }}>
-      Remove
-    </button>
-  </div>
-);
+class EditExpensePage extends React.Component {
+  onSubmit = ( oneExpense ) => {
+    this.props.onSubmit( this.props.expense.id, oneExpense );
+    this.props.history.push( '/' );
+  };
+
+  onClick = () => {      
+    const theId = this.props.expense.id;
+    // console.log( 'props expense', { id: theId } );
+    
+    this.props.onClick( theId );
+    this.props.history.push( '/' );
+  };
+  
+
+  render() {
+    return (
+      <div>
+        <ExpenseForm 
+          expense={ props.expense }
+          onSubmit={ this.onSubmit }
+        />
+        <button onClick={ this.onClick }>
+          Remove
+        </button>
+      </div>
+    );
+  };
+}
 
 const mapStateToProps = ( state, props ) => {
   return {
     expense: state.expenses.find( ( oneExpense ) => oneExpense.id === props.match.params.id )
   };
 };
+
+const mapDispatchToProps = ( dispatch ) => ( { 
+  onSubmit: ( theId, oneExpense ) => dispatch( editExpense( theId, oneExpense ) ),
+  onClick: ( theId ) => dispatch( removeExpense( { id: theId } ) )
+} );
 
 export default connect( mapStateToProps )( EditExpensePage );
