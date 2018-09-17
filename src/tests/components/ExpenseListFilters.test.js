a483490a3e25dc88753f797da51ab2ec75f33a35
Saadh <atextbooksituation@gmail.com>
2018-09-17 10:56:06 -0400

    Up to — Section 12, Lecture 126: "Testing ExpenseListFilters" <14:50> [00:45] — forgot to commit before next section

--- src/tests/components/ExpenseListFilters.test.js
+++ src/tests/components/ExpenseListFilters.test.js
@@ -5,10 +5,41 @@ import { ExpenseListFilters } from '../../components/ExpenseListFilters';
 
 import { filters, moreFilters } from '../fixtures/filters';
 
-let setStartDate, setEndDate, setTextFilter, sortByDate, sortByAmount;
 
-test( '', () => { 
+let wrapper, setStartDate, setEndDate, setTextFilter, sortByDate, sortByAmount;
 
+const changePropData = () => {
+  wrapper.setProps( { 
+    filters: moreFilters 
+  } );
+}
+
+beforeEach( () => {
+  setStartDate = jest.fn();
+  setEndDate = jest.fn();
+  setTextFilter = jest.fn();
+  sortByDate = jest.fn();
+  sortByAmount = jest.fn();
+
+  wrapper = shallow( <ExpenseListFilters 
+      filters={ filters }
+      setStartDate={ setStartDate }
+      setEndDate={ setEndDate }
+      setTextFilter={ setTextFilter }
+      sortByDate={ sortByDate }
+      sortByAmount={ sortByAmount }
+    />
+  );
+} );
+
+test( 'should render ExpenseListFilters correctly', () => { 
+  expect( wrapper ).toMatchSnapshot();
+} );
+
+test( 'should render ExpenseListFilters with alt data correctly', () => { 
+  changePropData();
+
+  expect( wrapper ).toMatchSnapshot();
 } );
 
 test( '', () => { 
