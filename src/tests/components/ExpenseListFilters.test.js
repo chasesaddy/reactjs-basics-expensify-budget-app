d2fcd500331a451479f23d29122138482a9fba4a
Saadh <atextbooksituation@gmail.com>
2018-09-17 07:36:21 -0400

    Part of — Section 12, Lecture 126: "Testing ExpenseListFilters" <14:50> [00:45] — Initial prep work for tests

---
+++ src/tests/components/ExpenseListFilters.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { ExpenseListFilters } from '../../components/ExpenseListFilters';
+
+import { filters, moreFilters } from '../fixtures/filters';
+
+let setStartDate, setEndDate, setTextFilter, sortByDate, sortByAmount;
+
+test( '', () => { 
+
+} );
+
+test( '', () => { 
+
+} );
