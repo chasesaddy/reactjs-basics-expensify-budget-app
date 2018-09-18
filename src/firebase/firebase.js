806ebe5fb5b343416ecc99405390797c645c78b8
Saadh <atextbooksituation@gmail.com>
2018-09-18 19:44:49 -0400

    Up to — Section 15, Lecture 155: "Creating a Separate Test Database" <21:15> [00:64] and also Section 15, Lecture 156: "Heroku Environment Variables" <6:38> [00:20]

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/database';
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyA-GOA3b-HvrFH3LKr37DvKw7OPrYBj3QI",
-  authDomain: "learning-js-expensify-deux.firebaseapp.com",
-  databaseURL: "https://learning-js-expensify-deux.firebaseio.com",
-  projectId: "learning-js-expensify-deux",
-  storageBucket: "learning-js-expensify-deux.appspot.com",
-  messagingSenderId: "365586213998"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp( firebaseConfig );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,13 @@
 const path = require( 'path' );
+const webpack = require( 'webpack' );
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+if ( process.env.NODE_ENV === 'test' ) {
+  require( 'dotenv' ).config( { path: '.env.test' } );
+} else if ( process.env.NODE_ENV === 'developement') {
+  require( 'dotenv' ).config( { path: '.env.development' } );
+};
 
 module.exports = {
   entry: './src/app.js',
@@ -20,10 +29,20 @@ module.exports = {
       ]
     }]
   },
+  plugins: [
+    new webpack.DefinePlugin( { 
+      'process.env.FIREBASE_API_KEY': JSON.stringify( process.env.FIREBASE_API_KEY ),
+      'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify( process.env.FIREBASE_AUTH_DOMAIN ),
+      'process.env.FIREBASE_DATABASE_URL': JSON.stringify( process.env.FIREBASE_DATABASE_URL ),
+      'process.env.FIREBASE_PROJECT_ID': JSON.stringify( process.env.FIREBASE_PROJECT_ID ),
+      'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET ),
+      'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID )
+    } )
+  ],
   devtool: 'cheap-module-eval-source-map',
   devServer: {
     contentBase: path.join( __dirname, 'public' ),
     historyApiFallback: true
   },
-  mode: 'development'
+  // mode: 'development'
 };
