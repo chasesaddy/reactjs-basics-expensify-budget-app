9c339d2a4ba2cad535972a8405c4d9a952cad60d
Saadh <atextbooksituation@gmail.com>
2018-09-22 11:17:08 -0400

    Up to — Section 13, Lecture 138: "New Feature Workflow" <12:32> [00:32]

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeraljs';
 
 const ExpenseListItem = ( { id, description, amount, createdAt } ) => (
   <div>
     <h3>
       <Link to={ `${ id }/edit` }>{ description }</Link>
     </h3>
-    <p>{ amount } - { createdAt }</p>    
+    <p>
+      { numeral( amount / 100 ).format( '$0,0.00' ) } 
+       -  
+      { moment( createdAt ).format( 'MMMM Do, YYYY' ) }
+    </p>
   </div>
 );
 
