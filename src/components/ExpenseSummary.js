c459068acf71dce7d0263dc91e9b6c398712f845
Saadh <atextbooksituation@gmail.com>
2018-09-23 09:19:27 -0400

    Up to — Section 13, Lecture 140: "Build It: Adding Summary Component" <18:39> [00:56] — I think some of the ways of Andrew’s is for sure better and deals with minimal changing. So did that. So a merger of my code and his

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -4,32 +4,30 @@ import numeral from 'numeraljs';
 
 import selectExpenses from '../selectors/expenses';
 import selectExpensesTotal from '../selectors/expenses-total';
-import { expensesCount } from '../selectors/expenses-total';
 
 export class ExpenseSummary extends React.Component {
-  count() {
-    return expensesCount( this.props.expenses );
-  }
   plural() {
-    return this.count() > 1 ? 'expenses' : 'expense';
+    return this.props.expenseCount > 1 ? 'expenses' : 'expense';
   }
   totalAmount() {
-    const amount = selectExpensesTotal( this.props.expenses );
-    return numeral( amount / 100 ).format( '$0,0.00' );
+    return numeral( this.props.expensesTotal / 100 ).format( '$0,0.00' );
   }
 
   render() {
     return (
       <div>
-        Viewing { this.count() } { this.plural() } totalling { this.totalAmount() }
+        Viewing { this.props.expenseCount } { this.plural() } totalling { this.totalAmount() }
       </div>
     );
   }
 }
 
 const mapStateToProps = ( state ) => {
-  return {    
-    expenses: selectExpenses( state.expenses, state.filters )
+  const visibleExpenses = selectExpenses( state.expenses, state.filters );
+
+  return {
+    expenseCount: visibleExpenses.length,
+    expensesTotal: selectExpensesTotal( visibleExpenses )
   };
 };
 
--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -3,13 +3,10 @@ import { shallow } from 'enzyme';
 
 import { ExpenseSummary } from '../../components/ExpenseSummary';
 
-import expenses from '../fixtures/expenses';
-
 test( 'should show display for single expense', () => { 
-  const example = [ expenses[ 0 ] ];
   const shouldBe = 'Viewing 1 expense totalling $3.50';
 
-  const wrapper = shallow( <ExpenseSummary expenses={ example } /> );
+  const wrapper = shallow(<ExpenseSummary expenseCount={ 1 } expensesTotal={ 350 } /> );
   const specificHtml = wrapper.find( 'div' ).at( 0 );
 
   expect( specificHtml.text() ).toBe( shouldBe );
@@ -17,10 +14,9 @@ test( 'should show display for single expense', () => {
 } );
 
 test( 'should show display for multiple expenses', () => { 
-  const example = expenses.slice( 0, 3 );
   const shouldBe = 'Viewing 3 expenses totalling $81.40';
 
-  const wrapper = shallow( <ExpenseSummary expenses={ example } /> );
+  const wrapper = shallow(<ExpenseSummary expenseCount={ 3 } expensesTotal={ 8140 } /> );
   const specificHtml = wrapper.find( 'div' ).at( 0 );
 
   expect( specificHtml.text() ).toBe( shouldBe );
