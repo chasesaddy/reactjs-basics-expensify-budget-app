443e787fcc3aa8f85948d68dbfa4ec0d6ea4fa1d
Saadh <atextbooksituation@gmail.com>
2018-09-23 12:05:27 -0400

    Forgot to check tests. One test needed updating.

--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -7,7 +7,7 @@ test( 'should show display for single expense', () => {
   const shouldBe = 'Viewing 1 expense totalling $3.50';
 
   const wrapper = shallow(<ExpenseSummary expenseCount={ 1 } expensesTotal={ 350 } /> );
-  const specificHtml = wrapper.find( 'div' ).at( 0 );
+  const specificHtml = wrapper.find( 'div h1' ).at( 0 );
 
   expect( specificHtml.text() ).toBe( shouldBe );
   expect( wrapper ).toMatchSnapshot();
@@ -17,7 +17,7 @@ test( 'should show display for multiple expenses', () => {
   const shouldBe = 'Viewing 3 expenses totalling $81.40';
 
   const wrapper = shallow(<ExpenseSummary expenseCount={ 3 } expensesTotal={ 8140 } /> );
-  const specificHtml = wrapper.find( 'div' ).at( 0 );
+  const specificHtml = wrapper.find( 'div h1' ).at( 0 );
 
   expect( specificHtml.text() ).toBe( shouldBe );
   expect( wrapper ).toMatchSnapshot();
